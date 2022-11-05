9d96b68577c1fe8ed6a53e5d64b9842511ea9c05
Juscélia de Souza <jusceliadesousa@gmail.com>
2022-11-05 16:08:46 -0300

    chore: rota do perfil

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -2,8 +2,19 @@ import { FastifyInstance } from "fastify";
 import { api } from "../api/axios";
 import { z } from "zod";
 import { prisma } from "../lib/prisma";
+import { authenticate } from "../plugins/authenticate";
 
 export async function authRoutes(fastify: FastifyInstance) {
+  fastify.get(
+    "/me",
+    {
+      onRequest: [authenticate],
+    },
+    async (request) => {
+      return { user: request.user };
+    }
+  );
+
   fastify.post("/users", async (request) => {
     const createUserBody = z.object({
       access_token: z.string(),
@@ -34,29 +45,32 @@ export async function authRoutes(fastify: FastifyInstance) {
     let user = await prisma.user.findUnique({
       where: {
         googleId: userInfo.id,
-      }
-    })
+      },
+    });
 
-    if(!user) {
+    if (!user) {
       user = await prisma.user.create({
         data: {
           googleId: userInfo.id,
           name: userData.name,
           email: userData.email,
-          avatarUrl: userInfo.picture
-        }
-      })
+          avatarUrl: userInfo.picture,
+        },
+      });
     }
 
     // Refresh token
 
-    const token = fastify.jwt.sign({
-      name: user.name,
-      avatarUrl: user.avatarUrl,
-    }, {
-      sub: user.id,
-      expiresIn:'7 days'
-    });
+    const token = fastify.jwt.sign(
+      {
+        name: user.name,
+        avatarUrl: user.avatarUrl,
+      },
+      {
+        sub: user.id,
+        expiresIn: "7 days",
+      }
+    );
 
     return { token };
   });
