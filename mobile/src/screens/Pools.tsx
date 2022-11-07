87739828e27bfd929ccf177deca40ea8fbcbcf72
Juscélia de Souza <jusceliadesousa@gmail.com>
2022-11-07 12:15:34 -0300

    chore: flatlist e list empty component

--- mobile/src/screens/Pools.tsx
+++ mobile/src/screens/Pools.tsx
@@ -1,13 +1,46 @@
-import { VStack, Icon } from "native-base";
+import { useEffect, useState } from "react";
+import { VStack, Icon, useToast, FlatList } from "native-base";
 import { useNavigation } from "@react-navigation/native";
+
+import { api } from "../services/api";
 
 import { Octicons } from "@expo/vector-icons";
 
 import { Button } from "../components/Button";
 import { Header } from "../components/Header";
+import { Loading } from "../components/Loading";
+import { PoolCard, PoolCardProps } from "../components/PoolCard";
+import { EmptyPoolList } from "../components/EmptyPoolList";
 
 export function Pools() {
-  const {navigate} = useNavigation();
+  const [isLoading, setIsLoading] = useState(true);
+  const [pools, setPools] = useState<PoolCardProps[]>([]);
+
+  const { navigate } = useNavigation();
+
+  const toast = useToast();
+
+  async function fetchPools() {
+    try {
+      setIsLoading(true);
+      const response = await api.get("/pools");
+      setPools(response.data.pools);
+    } catch (error) {
+      console.log(error);
+
+      toast.show({
+        title: "Não foi possível carregar os balões",
+        placement: "top",
+        bgColor: "red.500",
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchPools();
+  }, []);
 
   return (
     <VStack flex={1} bg="gray.900">
@@ -29,6 +62,21 @@ export function Pools() {
           onPress={() => navigate("find")}
         />
       </VStack>
+
+      {
+        isLoading ? <Loading /> :
+        <FlatList
+          data={pools}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => <PoolCard data={item} />}
+          px={5}
+          showsVerticalScrollIndicator={false}
+          _contentContainerStyle={{ pb: 10 }}
+          ListEmptyComponent={() => <EmptyPoolList />}
+        />
+      }
+
+      {/* <Loading /> */}
     </VStack>
   );
 }
