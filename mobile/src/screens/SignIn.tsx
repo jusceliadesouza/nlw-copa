cec329bdf35e62d26a01f3868a84184de768564e
Juscélia de Souza <jusceliadesousa@gmail.com>
2022-11-06 23:24:06 -0300

    chore: redirecionamento do usuário para as rotas da aplicação

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -8,7 +8,7 @@ import Logo from "../assets/logo.svg";
 import { Button } from "../components/Button";
 
 export function SignIn() {
-  const { signIn, user } = useAuth();
+  const { signIn, isUserLoading } = useAuth();
 
   // console.log('Dados do usuário: ', user)
 
@@ -22,6 +22,8 @@ export function SignIn() {
         type="SECONDARY"
         mt={12}
         onPress={signIn}
+        isLoading={isUserLoading}
+        _loading={{ _spinner: { color: 'white' } }}
       />
 
       <Text color="white" textAlign="center" mt={4}>
